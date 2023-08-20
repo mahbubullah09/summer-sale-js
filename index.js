d1df6036cc7d96f676713c158820b491a060eb1d
Mahbubullah <mahbubullahpathan@gmail.com>
2023-08-21 00:21:36 +0600

    price section complete 1.0

--- index.js
+++ index.js
@@ -15,38 +15,58 @@ function setName(name) {
 }
 
 
-function setPrice(itemPrice){
-    
-    const totalPriceId=document.getElementById('totalPrice');
-    const priceString=totalPriceId.innerText;
-    const price= parseFloat(priceString);
-   
-   
-   const totalPriceNumber= price+itemPrice;
-   const totalPrice=totalPriceNumber.toFixed(2)
+function setPrice(itemPrice) {
 
-    totalPriceId.innerText=totalPrice ;
+    const totalPriceId = document.getElementById('totalPrice');
+    const totalpriceString = totalPriceId.innerText;
+    const totalprice = parseFloat(totalpriceString);
 
-    
-    
-    const buttonId= document.getElementById('purchase-btn');
-    if(totalPriceNumber > 0){
+
+    const totalPriceNumber = totalprice + itemPrice;
+    const totalPriceFormated = totalPriceNumber.toFixed(2)
+
+    totalPriceId.innerText = totalPriceFormated;
+
+
+
+    // for price section 
+
+    const priceId = document.getElementById('price-Id');
+    const priceSreing = priceId.innerText;
+    const price = parseFloat(priceSreing);
+
+    const priceNumber = price + itemPrice;
+    const priceFormatted = priceNumber.toFixed(2);
+
+    priceId.innerText = priceFormatted;
+
+
+
+
+    // for puschase button disabvle 
+
+
+    const buttonId = document.getElementById('purchase-btn');
+    if (totalPriceNumber > 0) {
         buttonId.classList.remove('cursor-not-allowed')
         buttonId.classList.remove('bg-slate-500')
 
         buttonId.classList.add('cursor-pointer')
         buttonId.classList.add('bg-[#E527B2]')
-        
-        }
-    const applyId= document.getElementById('apply-btn');
-    if(totalPriceNumber >= 200){
+
+    }
+
+    // for apply button disabvle 
+
+    const applyId = document.getElementById('apply-btn');
+    if (totalPriceNumber >= 200) {
         applyId.classList.remove('cursor-not-allowed')
         applyId.classList.remove('bg-slate-500')
 
         applyId.classList.add('cursor-pointer')
         applyId.classList.add('bg-[#E527B2]')
-        
-        }
+
+    }
 
 
 
@@ -54,29 +74,41 @@ function setPrice(itemPrice){
 }
 
 
-function discount(){
-    const couponId=document.getElementById('couponField');
-    const couponValue= couponId.value ;
-    couponId.value='';
+function discount() {
+    const couponId = document.getElementById('couponField');
+    const couponValue = couponId.value;
+    couponId.value = '';
 
-    if(couponValue !== 'SELL200'){
+    if (couponValue !== 'SELL200') {
         return
     }
 
-    let discount=0;
+    let discount = 0;
     console.log(discount);
-    const totalPriceId=document.getElementById('totalPrice');
-    const priceString=totalPriceId.innerText;
-    const price= parseFloat(priceString);
-    console.log(price);
-
-    discount=((price*20)/100);
-    const discountFormated=discount.toFixed(2);
-
-    const discountId=document.getElementById('totalDiscount');
-    discountId.innerText=discountFormated ;
+    const totalPriceId = document.getElementById('totalPrice');
+    const priceString = totalPriceId.innerText;
+    const totalPrice = parseFloat(priceString);
    
-    
+
+    discount = ((totalPrice * 20) / 100);
+    const discountFormated = discount.toFixed(2);
+
+    const discountId = document.getElementById('totalDiscount');
+    discountId.innerText = discountFormated;
+
+    // for price section 
+
+    const priceId = document.getElementById('price-Id');
+    const priceSreing = priceId.innerText;
+    const price = parseFloat(priceSreing);
+
+    const priceNumber = price - discount;
+    const priceFormatted = priceNumber.toFixed(2);
+
+    priceId.innerText = priceFormatted;
+
+
+
 
 
 
@@ -90,8 +122,8 @@ function k_accesorries() {
     const priceId = document.getElementById('k-accesorries-price');
     const priceString = priceId.innerText;
     const priceOfItem = parseFloat(priceString);
-   
-  
+
+
 
     setName(nameOfItem);
     setPrice(priceOfItem);
